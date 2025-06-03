374498bfffd9a6b2f0cf81588cb93f0f784ed7f3
Michael Garthe <m.garthe@vald.com>
2025-06-03 20:11:39 +1000

    changes to frontend

--- my-app/src/TodoList.tsx
+++ my-app/src/TodoList.tsx
@@ -2,25 +2,25 @@ import axios from "axios"
 import { useState } from "react"
 
 export const TodoList = () => {
-    const [todoItems, setTodoItems] = useState<string[]>([]);
+    const [todoItems, setTodoItems] = useState<TodoistItem[]>([]);
     const [todoInput, setTodoInput] = useState<string>('');
     const [hasLoadedOnce, setHasLoadedOnce] = useState<boolean>(false);
 
     const getTodoList = () => axios.get<TodoistItem[]>("https://localhost:7044/todo").then(r => {
-        setTodoItems(r.data.map(t => t.text))
+            setTodoItems(r.data.map(t => ({text: t.text, id: t.id})))
         })
         .catch(e => {
             console.log(e)
         });
 
-    const addTodo = () => axios.post('https://localhost:7044/todo', {text: todoInput})
+    const addTodo = () => axios.post('https://localhost:7044/todo', { text: todoInput })
     .then(x => {
         setTodoInput('');
         getTodoList();
     })
     .catch(e => console.log(e));
 
-    const completeTodoListItem = (todoText: string) => axios.delete('https://localhost:7044/todo', {data: {text: todoText}}).then(r => getTodoList()).catch(e => console.log(e));
+    const completeTodoListItem = (id: number) => axios.delete('https://localhost:7044/todo', {data: {id}}).then(r => getTodoList()).catch(e => console.log(e));
 
 
     if (!hasLoadedOnce) {
@@ -33,13 +33,14 @@ export const TodoList = () => {
         <h1>Todo List</h1>
         <input value={todoInput} onChange={e => setTodoInput(e.target.value)} />
         <button onClick={() => addTodo()}>Add</button>
-        {todoItems.map(t => <TodoListItem text={t}  completeItem={() => completeTodoListItem(t)}/>)}
+        {todoItems.map(t => <TodoListItem text={t.text}  completeItem={() => completeTodoListItem(t.id)}/>)}
         </div>
     )
 }
 
 export interface TodoistItem {
-    text: string
+    id: number;
+    text: string;
 }
 
 
@@ -50,7 +51,7 @@ interface TodoListItemProps {
 export const TodoListItem = (props: TodoListItemProps) => {
 
     return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'row'}}>
         <button onClick={() => props.completeItem()}>Complete</button>
         <p>{props.text}</p>
     </div>
